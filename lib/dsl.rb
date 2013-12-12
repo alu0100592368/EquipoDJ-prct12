939b58d3e2e1e23adc860e4ea924ddb6018e52c2
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-12-12 21:30:43 +0000

    añadiendo requiere

--- lib/dsl.rb
+++ lib/dsl.rb
@@ -1,3 +1,8 @@
 filename = ARGV.shift
 
 calculo = File.open(filename).read
+calculo = <<"CALCULO"
+        require './lib/matrixlpp.rb'
+        MatrixDSL.new() do
+                #{calculo}
+        end
