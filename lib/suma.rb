400910bb63bb80c5bb4e1121fc9a85abad89f4cf
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-12-12 23:49:21 +0000

    completando operacion suma

--- lib/suma.rb
+++ lib/suma.rb
@@ -1,3 +1,13 @@
 #Tipo de operacion
 operacion "suma"
 
+#Muestra por consola
+opcion "consola"
+
+#Matrices dispersas
+opcion "dispersa"
+operando [1,0],[2,0]
+
+#Matrices densas
+opcion "densa"
+operando [1,2],[2,3]
