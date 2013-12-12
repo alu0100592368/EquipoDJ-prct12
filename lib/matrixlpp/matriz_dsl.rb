bc8d899594403cf2c71bb2f916e8b5b654f9d9dd
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-12-12 23:30:22 +0000

    definiendo initialize

--- lib/matrixlpp/matriz_dsl.rb
+++ lib/matrixlpp/matriz_dsl.rb
@@ -1,5 +1,12 @@
 class MatrixDSL
-
+  #Inicializa los valores a los que se les pase por parametro
+  def initialize(operation = "", &block)
+    @name= operation
+    @MatrizClass = ""
+    @salida = 0 
+    @Matriz = []
+    instance_eval &block
+  end
        
 
 end
